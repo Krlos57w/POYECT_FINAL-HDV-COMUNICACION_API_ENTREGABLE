f614597ead4e5fae2130bde25908e247d827f5f1
Krlos57w <k_rlos57@yahoo.com>
2023-11-14 17:14:32 -0500

    proyecto_final_progweb_cambioshdv

--- src/js/cardbooks.js
+++ src/js/cardbooks.js
@@ -1,48 +1,40 @@
 /*logica para la cards de todos las piezas */
-//------------------------------MOSTRAR LIBRO CUARDADO EN LA API-----------------------------------
+//------------------------------MOSTRAR PIEZA GUARDADO EN LA API-----------------------------------
 //escuchador de eventos
-document.addEventListener('DOMContentLoaded', getLibros);//cuando se cargue la pagina se ejecuta la funcion
+document.addEventListener('DOMContentLoaded', getPiezas);//cuando se cargue la pagina se ejecuta la funcion
 
-
-function getLibros() {//funcion para traer los datos 
-    fetch('http://localhost:3000/books')//fetch se usa para hacer peticiones http
-        .then(Response => Response.json())//se convierte la respuesta en json, then se usa para ejecutar una funcion cuando se cumpla la promesa
-        .then(data => mostrarLibros(data))// 
-        .catch(
-            function (error) {
-                console.log(error);
-            });
+function getPiezas() {//funcion para obtener los datos de la api
+    fetch('http://localhost:3000/books')//obtenemos los datos de la api
+        .then(response => response.json())
+        .then(data => mostrarPiezas(data))//llamamos a la funcion para mostrar los datos
+        .catch(error => {
+            console.log(error);
+            // Puedes agregar aquí código para manejar el error, como mostrar un mensaje al usuario.
+        });
 }
 
-//ingresar datos en el card y mostrarlos en html
-function mostrarLibros(listLibros) {
-    let datosLibrosFila = document.getElementById("cardBooks");
-    let libros = "";
-    for (var cont = 0; cont < listLibros.length; cont++) {
-
-        
-
-            let datosFila = `<div class="card mb-3 bg-dark-subtle border border-2 border-dark" style="max-width: 800px;">
+function mostrarPiezas(listPiezas) {
+    let datosPiezasFila = document.getElementById("cardBooks");
+    let piezas = "";
+    for (var cont = 0; cont < listPiezas.length; cont++) {
+        let datosFila = `<div class="card mb-3 bg-dark-subtle border border-2 border-dark" style="max-width: 800px;">
                             <div class="row g-0">
                                 <div class="col-md-4">
-                                <img src="src/pages/piezas_mecanicas.jpg" class="card-img-top" alt="libro">
+                                <img src="src/pages/piezas_mecanicas.jpg" class="card-img-top" alt="pieza">
 
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body">
-                                        <h6 class="card-title"> NOMBRE PIEZA: ${listLibros[cont].nombreP} </h6>
-                                        <p class="card-text"> FABRICANTE:  ${listLibros[cont].makerPieza} </p>
-                                        <p class="card-text"> ESTADO DE PIEZA:  ${listLibros[cont].statePieza} </p>
-                                        <p class="card-text"> ID PIEZA:  ${listLibros[cont].id} </p>
+                                        <h6 class="card-title"> NOMBRE PIEZA: ${listPiezas[cont].nombreP} </h6>
+                                        <p class="card-text"> FABRICANTE:  ${listPiezas[cont].makerPieza} </p>
+                                        <p class="card-text"> ESTADO DE PIEZA:  ${listPiezas[cont].statePieza} </p>
+                                        <p class="card-text"> ID PIEZA:  ${listPiezas[cont].id} </p>
                                     </div>
                                 </div>
                             </div>
                         </div>`;
 
-
-            
-            libros += datosFila;
-        
+        piezas += datosFila;
     }
-    datosLibrosFila.innerHTML = libros
+    datosPiezasFila.innerHTML = piezas;
 }
